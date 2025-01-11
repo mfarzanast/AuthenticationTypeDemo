589fe6979c486cd4a398f614bf63ee7174034b03
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2025-01-11 19:51:37 +0530

    Add project files.

---
+++ AuthenticationTypes/AuthHandlers/BasicAuthHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace AuthenticationTypes.AuthHandlers
+{
+    public class BasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) 
+            :base(options,logger,encoder,clock)
+        {
+              
+        }
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Missing Authentication Headers"));
+            }
+
+            try {
+
+                var authHeader = Request.Headers["Authorization"].ToString();
+                var endocdedCreds = authHeader.Substring("Basic".Length).Trim();
+                var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(endocdedCreds)).Split(':');
+                var username = credentials[0];
+                var password = credentials[1];
+
+                if (username == "user" && password == "pass")
+                {
+                    var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                    var claimIdentity = new ClaimsIdentity(claims, Scheme.Name);
+                    var principal = new ClaimsPrincipal(claimIdentity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return Task.FromResult(AuthenticateResult.Success(ticket));
+                }
+                else {
+                    return Task.FromResult(AuthenticateResult.Fail("Invlaid Username or password"));
+                }
+
+            }
+            catch (Exception ex) {
+                return Task.FromResult(AuthenticateResult.Fail("Invlaid Authentication"));
+            }
+        }
+    }
+}
---
+++ AuthenticationTypes/Program.cs
@@ -0,0 +1,59 @@
+using AuthenticationTypes.AuthHandlers;
+using Microsoft.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthHandler>("BasicAuthentication", null);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("basic", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "basic",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Enter your username and password as 'username:password', encoded in Base64."
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "basic"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
