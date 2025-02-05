0d0f26ca165b491531cddd0ba4b52f3944f9ccb6
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2025-02-05 14:31:49 +0530

    Add token based authentication

---
+++ AuthenticationTypes/Controllers/TokenAuthController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationTypes.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class TokenAuthController : ControllerBase
+    {
+        private readonly  IConfiguration _configuration;
+        public TokenAuthController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginModel user)
+        {
+            if (user.UserName == "admin" && user.Password == "Password")
+            {
+                var token = GenerateJwtToken(user.UserName);
+                return Ok(new { token });
+            }
+            return Unauthorized();
+            
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken
+            (
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials:credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- AuthenticationTypes/Program.cs
+++ AuthenticationTypes/Program.cs
@@ -1,5 +1,11 @@
 using AuthenticationTypes.AuthHandlers;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.Xml;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,38 +44,86 @@ builder.Services.AddSwaggerGen(c =>
     });
 }); */
 #endregion
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "X-Api-Key",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-        Scheme = "ApiKeyScheme",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Api key needed to access the endpoints. Example: X-Api-Key."
-    });
 
-    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "ApiKey"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+#region APIKEy based auth 
+
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+//    {
+//        Name = "X-Api-Key",
+//        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+//        Scheme = "ApiKeyScheme",
+//        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+//        Description = "Api key needed to access the endpoints. Example: X-Api-Key."
+//    });
+
+//    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+//    {
+//        {
+//            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+//            {
+//                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+//                {
+//                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+//                    Id = "ApiKey"
+//                }
+//            },
+//            new string[] {}
+//        }
+//    });
+//});
+#endregion
 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(options => {
+
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme { 
+    
+        Name="Authorization",
+        Type=SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In=ParameterLocation.Header,
+        Description="Enter bearer followed by your token"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+
+        { 
+            new OpenApiSecurityScheme{ 
+            
+                Reference = new OpenApiReference{ 
+                    Type= ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{ }
+        }
+    });
+});
+
+//Configure JWT token
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+
+    options.TokenValidationParameters = new TokenValidationParameters { 
+    
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
 
 var app = builder.Build();
 
@@ -80,7 +134,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<ApiKeyHandler>();
+//app.UseMiddleware<ApiKeyHandler>();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
